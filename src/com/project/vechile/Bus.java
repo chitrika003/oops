268bf66c7ad895e243a1d7805ddcc56c9288bbb1
--chitrika003 <--cgchitrika@gmail.com>
2026-01-26 13:27:22 +0530

    Bus class overrides  run method

--- src/com/project/vechile/Bus.java
+++ src/com/project/vechile/Bus.java
@@ -1,13 +1,16 @@
 package com.project.vechile;
 
+import java.util.Scanner;
+
 public class Bus extends AbstractVechile {
+
     private String color;
     private String bus_number;
     private int capacity;
 
     @Override
     void run() {
-
+        System.out.println("The Bus Runs on Diesel");
     }
 
     @Override
